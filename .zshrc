8374d7320a4da6d2e74fa9dec1c4e7636ff4aa6d
QB1 <travissimpson@Traviss-iMac.attlocal.net>
2025-03-21 13:24:05 -0500

    Cleaned up Git tracking

---
+++ .zshrc
@@ -0,0 +1,4 @@
+export PATH="/usr/local/bin:$PATH"
+alias python="python3"
+export PATH="/usr/local/opt/python@3.11/bin:$PATH"
+source ~/my_env/bin/activate
---
+++ test.py
@@ -0,0 +1,4 @@
+import numpy as np
+import speech_recognition as sr
+print("NumPy Version:", np.__version__)
+print("SpeechRecognition Version:", sr.__version__)
